00ecb31a5609fac6e5a96194990852377dbff6f9
vladnovoren <vladnovoren@gmail.com>
2021-07-09 18:12:55 +0500

    1234

--- list.h
+++ list.h
@@ -1,5 +1,22 @@
 #ifndef LIST_H
 #define LIST_H
 
+#include <cstddef>
+#include "list_config.h"
+
+struct List {
+    size_t n_elems;
+    size_t capacity;
+    ListElemT* elems;
+};
+
+enum ListErrors {
+    LIST_NO_ERRORS,
+    LIST_ALLOC_ERROR
+};
+
+
+int 
+
 
 #endif /* list.h */
---
+++ list_config.h
@@ -0,0 +1,8 @@
+#ifndef LIST_CONFIG_H
+#define LIST_CONFIG_H
+
+
+typedef int ListElemT;
+
+
+#endif /* list_config.h */

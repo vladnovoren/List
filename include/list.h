202e753b5b021120b13524a6692ab2234a39b4b8
vladnovoren <47698772+vladnovoren@users.noreply.github.com>
2021-08-20 03:38:04 +0500

    Update list.h

--- include/list.h
+++ include/list.h
@@ -42,41 +42,45 @@ enum ListErrors {
 const ListElemT EMPTY_LIST_ELEM = {};
 
 
-void      List_ConnectNodes(List* list, const size_t first_phys_id, const size_t second_phys_id);
+void   List_ConnectNodes(List* list, const size_t first_phys_id, const size_t second_phys_id);
 
-void      List_ConnectArray(List* list, const size_t begin_phys_id, const size_t end_phys_id);
+void   List_ConnectArray(List* list, const size_t begin_phys_id, const size_t end_phys_id);
 
-int       List_CheckAndUpdateCapacity(List* list);
+int    List_CheckAndUpdateCapacity(List* list);
 
-int       List_PushFront(List* list, const ListElemT new_elem, size_t* phys_id = nullptr);
+int    List_PushFront(List* list, const ListElemT new_elem, size_t* phys_id = nullptr);
 
-int       List_PushBack(List* list, const ListElemT new_elem, size_t* phys_id = nullptr);
+int    List_PushBack(List* list, const ListElemT new_elem, size_t* phys_id = nullptr);
 
-int       List_Alloc(List* list);
+int    List_Alloc(List* list);
 
-void      List_Destruct(List* list);
+void   List_Destruct(List* list);
 
-int       List_Clear(List* list);
+int    List_Clear(List* list);
 
-int       List_GetPhysId(List* list, size_t logic_id, size_t* phys_id);
+int    List_GetPhysId(List* list, size_t logic_id, size_t* phys_id);
 
-int       List_GetByLogicId(List* list, size_t logic_id, ListElemT* found);
+int    List_GetByLogicId(List* list, size_t logic_id, ListElemT* found);
 
-int       List_GetByPhysId(List* list, const size_t phys_id, ListElemT* found);
+int    List_GetByPhysId(List* list, const size_t phys_id, ListElemT* found);
 
-int       List_InsertBefore(List* list, const size_t phys_id, const ListElemT new_elem);
+size_t List_GetNextPhysId(List* list, size_t phys_id);
 
-int       List_InsertAfter(List* list, const size_t phys_id, const ListElemT new_elem);
+size_t List_GetPrevPhysId(List* list, size_t phys_id);
 
-int       List_EraseByPhysId(List* list, size_t phys_id);
+int    List_InsertBefore(List* list, const size_t phys_id, const ListElemT new_elem);
 
-int       List_EraseByLogicId(List* list, size_t logic_id);
+int    List_InsertAfter(List* list, const size_t phys_id, const ListElemT new_elem);
 
-int       List_PopFront(List* list);
+int    List_EraseByPhysId(List* list, size_t phys_id);
 
-int       List_PopBack(List* list);
+int    List_EraseByLogicId(List* list, size_t logic_id);
 
-ListElemT List_Find(List* list, ListElemT elem, size_t* phys_id = nullptr, size_t* logic_id = nullptr);
+int    List_PopFront(List* list);
+
+int    List_PopBack(List* list);
+
+size_t List_Find(List* list, const ListElemT elem);
 
 
 
